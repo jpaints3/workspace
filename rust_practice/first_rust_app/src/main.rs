17065733137cd5532d05d63c72d619e98a867e86
Josiah <jopaintsil@gmail.com>
2024-01-24 15:29:36 -0800

    feat: Make input variable mutable and test it out.

--- rust_practice/first_rust_app/src/main.rs
+++ rust_practice/first_rust_app/src/main.rs
@@ -1,6 +1,12 @@
 mod print; // creates a new module using the print file
 fn main() {
-    let input = "Hello World from main file"; // Creates a string
-                                              // Strings have references at creation. So the text here is &str.
+    let mut input = "Hello World from main file"; // Creates a string
+                                                  // Strings have references at creation. So the text here is &str.
+                                                  // All variables in rust are IMMUTABLE.
+                                                  // So by putting mut after let I are specifying that this variable is mutable.
     print::print_input(input); // Run the print_input function from the print module
+
+    input = "The world has ended.";
+
+    print::print_input(input);
 }
