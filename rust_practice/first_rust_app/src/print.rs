b9964086421e6058d56de230f3fe1ed2fd9019f7
Josiah <jopaintsil@gmail.com>
2024-01-24 16:17:58 -0800

    feat: Make a mutable String version of the input. Make it say 'Twice!'

--- rust_practice/first_rust_app/src/print.rs
+++ rust_practice/first_rust_app/src/print.rs
@@ -4,4 +4,11 @@ pub fn print_input(input: &str) {
     // The type of a string variable would likely be &String. This is when you use the String::from() method.
     println!("{input}"); //Runs the print new line method with the given variable input
                          // The ! after println signifies macros. I will learn more about that later.
+
+    let mut string_input: String = String::from(input); // Creates a new string of String type from the input
+
+    string_input.push_str(" Twice!"); // We can modify the string by using push_str
+                                      // We need to specify that the string is mutable before we can use this method.
+
+    println!("{string_input}")
 }
